1f1bff64329e0e33f81cf93a8506cad83c6bccc6
Heather Carminati <38896836+hcarminati@users.noreply.github.com>
2023-11-20 21:32:13 -0500

    Revert "a6"

--- app.js
+++ app.js
@@ -6,38 +6,16 @@ import AssignmentRoutes from "./assignment_service/routes.js";
 import Hello from "./hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
-import mongoose from "mongoose";
-import UserRoutes from "./users/routes.js";
-import "dotenv/config";
-import session from "express-session";
-
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
-mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(express.json());
 app.use(
     cors({
              credentials: true,
-             origin: process.env.FRONTEND_URL,
+             origin: "*",
          })
 );
-const sessionOptions = {
-    secret: "any string",
-    resave: false,
-    saveUninitialized: false,
-};
-if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-        sameSite: "none",
-        secure: true,
-    };
-}
-app.use(session(sessionOptions));
-
 app.use(express.json());
-UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 AssignmentRoutes(app);
